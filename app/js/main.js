ce82c74be53d2d709f79812de23394e37f22b5e1
Max Kokorin <kokorin.max@gmail.com>
2015-07-03 04:55:52 +0600

    Fixed untracked files

--- app/js/main.js
+++
@@ -1,3 +0,0 @@
-console.log('Go ahead for develop!');
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uY29mZmVlIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBLE9BQU8sQ0FBQyxHQUFSLENBQVksdUJBQVosQ0FBQSxDQUFBIiwiZmlsZSI6Im1haW4uanMiLCJzb3VyY2VSb290IjoiL3NvdXJjZS8iLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyAnR28gYWhlYWQgZm9yIGRldmVsb3AhJyJdfQ==
