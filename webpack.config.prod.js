5bb68326cb6e0951b706cc54d6b68de799620b37
Max Kokorin <kokorin.max@gmail.com>
2016-02-08 00:20:13 +0600

    Fix css loader for prod webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -39,12 +39,12 @@ module.exports = {
 	module: {
 		loaders: [
 			{ test: /\.js$/, loaders: ['babel'], include: path.join(__dirname, 'src/scripts') },
-			{ test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css') },
+			{ test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css', {publicPath: "../"}) },
 			{ test: /\.(png|jpg)$/, loader: 'file?name=/src/images/[name].[ext]' },
-			{ test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'file?name=/fonts/[name].[hash].[ext]&mimetype=application/font-woff'},
-			{ test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,loader: 'file?name=/fonts/[name].[hash].[ext]&mimetype=application/font-woff'}, 
-			{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file?name=/fonts/[name].[hash].[ext]&mimetype=application/octet-stream'}, 
-			{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file?name=/fonts/[name].[hash].[ext]'}, 
+			{ test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'file?name=fonts/[name].[hash].[ext]&mimetype=application/font-woff'},
+			{ test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,loader: 'file?name=fonts/[name].[hash].[ext]&mimetype=application/font-woff'}, 
+			{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file?name=fonts/[name].[hash].[ext]&mimetype=application/octet-stream'}, 
+			{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file?name=fonts/[name].[hash].[ext]'}, 
 			{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file?name=/images/[name].[hash].[ext]&mimetype=image/svg+xml' }
 		]
 	}
