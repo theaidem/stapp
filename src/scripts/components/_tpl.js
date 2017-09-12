8f5470c2c5ab2bae1fd2d9da9fcae57fbd7364d4
Max Kokorin <kokorin.max@gmail.com>
2017-09-12 09:48:44 +0600

    template component

---
+++ src/scripts/components/_tpl.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+
+class tpl extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    componentWillMount = () => {}
+
+    componentDidMount = () => {}
+
+    componentWillReceiveProps = (nextProps) => {}
+
+    shouldComponentUpdate = (nextProps, nextState) => {
+        return true
+    }
+
+    componentWillUpdate = (nextProps, nextState) => {}
+
+    componentDidUpdate = (nextProps, nextState) => {}
+
+    componentWillUnmount = () => {}
+
+    render() {
+        return null
+    }
+
+}
+
+const mapStateToProps = (state, props) => {
+    return {}
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(tpl)
