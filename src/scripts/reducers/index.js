defa822bdf4704dc151ed497b195ab46c7adb74f
Max Kokorin <kokorin.max@gmail.com>
2017-10-10 13:47:03 +0600

    added action reducer

--- src/scripts/reducers/index.js
+++ src/scripts/reducers/index.js
@@ -2,9 +2,12 @@ import app from './app'
 import {combineReducers} from 'redux'
 import {routerReducer} from 'react-router-redux'
 
+// eslint-disable-next-line no-unused-vars
+const action = (state = null, action) => (action.type)
+
 const rootReducer = combineReducers({
     routing: routerReducer,
-    app
+    app, action
 })
 
 export default rootReducer
